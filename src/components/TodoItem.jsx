083340680f242569f7aee6a82d051f5594cb8b0c
Isaac Alejandro Barco Cedeño <isaacbarco23@hotmail.com>
2025-12-14 00:03:43 -0500

    Completando y eliminando TODOs

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,9 +3,25 @@ import '../styles/TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className={`TodoItem-icon ${props.completed ? 'TodoItem-icon--check' : ''}`}>V</span>
-      <p className={`TodoItem-text ${props.completed ? 'TodoItem-text--completed' : ''}`}>{props.text}</p>
-      <span className="TodoItem-icon TodoItem-icon--delete">X</span>
+      <span
+        className={`TodoItem-icon ${props.
+        completed && 'TodoItem-icon--check'}`}
+        onClick={props.onComplete}
+      >
+        V
+      </span>
+      <p
+        className={`TodoItem-text ${props.
+        completed && 'TodoItem-text--completed'}`}
+      >
+        {props.text}
+      </p>
+      <span
+        className="TodoItem-icon TodoItem-icon--delete"
+        onClick={props.onDelete}
+      >
+        X
+      </span>
     </li>
   );
 }
